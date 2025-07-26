9a77d8c747fd36a69ee0a99a0f5f518d61738f03
Dev Saxena <devsaxena7668@gmail.com>
2025-07-26 22:26:00 +0530

    changes updated

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const dotenv = require('dotenv')
 dotenv.config({path: './config.env'})
 
+const dbconfig = require('./config/dbConfig')
 const app = require('./index') // to import the app from another file
                       
 const port = process.env.PORT_NUMBER || 3000;
