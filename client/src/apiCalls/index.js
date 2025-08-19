a4e344094c83aded4f4db91dd3a0e10abe3a36e0
Dev Saxena <devsaxena7668@gmail.com>
2025-08-19 23:50:34 +0530

    changes done

--- client/src/apiCalls/index.js
+++ client/src/apiCalls/index.js
@@ -1,7 +1,5 @@
 import axios from "axios"
 
-export const url = "http://localhost:5000"
-
 export const axiosInstance = axios.create({
     headers: {
         authorization: `Bearer ${localStorage.getItem('token')}`
--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
 const authRouter = require('./controllers/authController')
@@ -6,6 +7,7 @@ const userRouter = require('./controllers/userController')
 const chatRouter = require('./controllers/chatController')
 const messageRouter = require('./controllers/messageController')
 
+app.use(cors())
 app.use(express.json({
     limit: "50mb"
 }));
