18b34d9088a9fc983c361e2966d5d07594b7742d
Dev Saxena <devsaxena7668@gmail.com>
2025-08-26 11:49:04 +0530

    completed

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -81,7 +81,7 @@ import Chat from './components/Chat'
 import { useSelector } from 'react-redux'
 import {io} from 'socket.io-client'
 
-const socket = io('http://localhost:5000' , {
+const socket = io('https://chit-chat-app-x9g0.onrender.com' , {
   transports: ["websocket"],
   withCredentials: true
 })
