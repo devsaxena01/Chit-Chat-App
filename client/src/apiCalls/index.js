265b6e231922d51e2931e555071871039ecaf0e9
Dev Saxena <devsaxena7668@gmail.com>
2025-08-26 12:19:20 +0530

    completed Done

--- client/src/apiCalls/index.js
+++ client/src/apiCalls/index.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL:"http://localhost:5000",
+    baseURL:"https://chit-chat-app-x9g0.onrender.com",
     headers: {
         authorization: `Bearer ${localStorage.getItem('token')}`
     }
